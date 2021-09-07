4207f7c15f843f43e48221acd8fbcf23d7d17cc5
lamar321 <84905733+lamar321@users.noreply.github.com>
2021-09-07 22:49:53 +0100

    Update join.js

--- src/commands/info_commands/join.js
+++ src/commands/info_commands/join.js
@@ -66,9 +66,9 @@ module.exports = function run (guild, config)
    );
    */
 
-   return defaultChannel.send(`Hello, I'm RITA. Thanks for inviting me.`, {"embed": {
+   return defaultChannel.send(`Hello, I'm adembot. Thanks for inviting me.`, {"embed": {
       "color": 9514728,
-      "description": "We developed RITA to be the best free Translator Bot on Discord (& soon Guilded).\nSpeaking to other people should not have to cost you an arm and a leg, Our aim is to break that language barrier without you having to pay out for the privilege.",
+      "description": "adem devlopped me :)",
       "fields": [
          {
             "name": ":tada: Getting Started",
@@ -76,22 +76,22 @@ module.exports = function run (guild, config)
          },
          {
             "name": ":tools: Support ",
-            "value": "If you need some support why not drop by our [Support Server](https://discord.gg/hXaedzCQ8d) and we will be more than willing to lend a hand."
+            "value": "If you need some support why not drop by our [Server](https://discord.gg/4kQFEQ4Cst) and we will be more than willing to lend a hand."
          },
          {
             "name": ":lock: Permissions",
-            "value": `Rita has many functions, These all need different permissions to work. To check that RITA has ll the permissions she needs to functions correctly please use the **!tr checkperms bot** command.`
+            "value": `adembot has many functions, These all need different permissions to work. To check that adem has ll the permissions she needs to functions correctly please use the **!tr checkperms bot** command.`
          },
          {
             "name": ":moneybag: On a side note.",
-            "value": `While rita is free, and we always aim to keep it this way, She does have costs.\nCurrently the Dev Team pays these cost. If you would like to support us and enable us to continue to provide RITA for free then please visit our [GitHub Sponsors](https://github.com/sponsors/RitaBot-Project) page, or type **${config.translateCmdShort} donate** for more info`
+            "value": `adembot is free`
          }
       ],
 
       "footer": {
-         "text": "RITA is developed by the RITA Bot Project"
+         "text": "addembot is developed by adem"
       },
-      "title": "A MESSAGE FROM THE DEV TEAM"
+      "title": "hey"
    }});
 
 };
@@ -101,7 +101,7 @@ module.exports.newBot = function newBot (data)
 
    data.message.channel.send({"embed": {
       "color": 9514728,
-      "description": "Let's get started:\nRita supports a few different methods of translations.\n**‏‏‎ ‎‎‎‎‎‎**",
+      "description": "Let's get started:\nadem supports a few different methods of translations.\n**‏‏‎ ‎‎‎‎‎‎**",
       "fields": [
          {
             "name": ":flag_white: Translate by Reacting",
@@ -137,9 +137,9 @@ module.exports.newBot = function newBot (data)
       ],
 
       "footer": {
-         "text": `RITA is developed by the RITA Bot Project`
+         "text": `adembot is developed by the adem`
       },
-      "title": "THANK YOU FOR CHOOSING TO USE RITA."
+      "title": "THANK YOU FOR CHOOSING TO USE adembot."
    }});
 
 };
