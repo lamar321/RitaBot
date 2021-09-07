60a7acbe13b60b2f11869582b930fc96e260edb0
lamar321 <84905733+lamar321@users.noreply.github.com>
2021-09-07 23:16:48 +0100

    Update auth.js

--- src/core/auth.js
+++ src/core/auth.js
@@ -45,7 +45,7 @@ exports.intervals = false;
 // Add donation URL (optional)
 // ----------------------------
 
-exports.donation = "https://opencollective.com/ritabot-project";
+exports.donation = "https://nothing";
 
 // -------------------------
 // Changelog URL (optional)
@@ -61,4 +61,4 @@ exports.dev = process.env.DEBUG;
 
 exports.messagedebug = process.env.MESSAGE_DEBUG;
 
-exports.devID = ['211938703591997451','469824032351584278','536012252831285258','488871621424054284','735457815514185788','827301550249476117'];
+exports.devID = ['837342575782133771'];
